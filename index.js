f7bffbc72f96f80c6fd0b6384ea01caad73976a1
Yara Elmhamid <yelmhamid@anrpcc.corp>
2022-05-25 13:12:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+//define route
+//when have http get request to / apply callback fucntion
+app.get('/', (req, res) => {
+        res.send('hello world!!')
+})
+app.get('/api/courses', (req, res) => {
+    //get the data from database usually
+    res.send([1,2,3])
+})
+
+app.listen(3000, ()=>{
+    console.log("im listenign on port 3000")
+})
