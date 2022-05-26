d35da23a902ca29c1941cfc9ad19e82fad21c726
Yara Elmhamid <yelmhamid@anrpcc.corp>
2022-05-26 15:19:37 +0100

    tested the get method works with retreiving

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,4 +1,5 @@
-information about connection
+information about connection:
+-----------------------------
 used aiesec email
 user: yara_elmhamid@soljit.com
 pw: Astaghfirullah00
@@ -6,7 +7,11 @@ security token: zrOmdw4gerCKoN37qZ2bFqH1
 pw+sec = Astaghfirullah00zrOmdw4gerCKoN37qZ2bFqH1 
 client id(consumer key) = 3MVG9FMtW0XJDLd0N5pIRGGTmB5L9gIAwRcHcb6sJ6Sq6o7byO3QGcsZn8_SqzpigJF2If8WaC6iy3bntvyX7
 client secret  = 628B59237DD973AD77ADE212614680026E6C27970F7F38E24A0EC1D94C007284
-
 callback url http://localhost:1000/zrOmdw4gerCKoN37qZ2bFqH1 
+
+Ressources:
+-----------
+
+SF Methods: https://jsforce.github.io/jsforce/doc/Connection.html
 reset sec token:   https://help.salesforce.com/s/articleView?id=sf.user_security_token.htm&type=5
 enable api access: https://ebstalimited.zendesk.com/hc/en-us/articles/229295368-Enable-API-access-in-Salesforce
--- server.js
+++ server.js
@@ -25,50 +25,96 @@ var conn = new jsforce.Connection({
   redirectUri : 'http://localhost:1000/zrOmdw4gerCKoN37qZ2bFqH1'
 });
 
-//Connection code without GET HTTP request
-//Username and Password Login
-
-conn.login(username, password, function(err, acc) {
-  if (err) { return console.error(err); }
-  conn.query('SELECT Id, Name FROM Account', function(err, acc) {
+//###################################### User authentification.################################
+//GET HTTP request to Login information retreiving
+app.get('/forceapi/auth', (req, res) => {
+  conn.login(username, password, function(err, userInfo) {
     if (err) { return console.error(err); }
-    console.log("authentification worked, and here is  response of it")
-    console.log(acc);
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    //console.log("Access token: " + conn.accessToken);
+    //console.log("Instance URL token: " + conn.instanceUrl);
+    // logged in user property
+    //console.log("User ID: " + userInfo.id);
+    //console.log("Org ID: " + userInfo.organizationId);
+
+    res.send(userInfo); //inside login
   });
-});
+})
 
 
+//###################################### SOQL:  Fetch Account's records.################################
+//using SOQL connexion#query(soql, callbackopt)
+
+//SIMPLE QUERY
+app.get('/forceapi/Records1', (req, res) => {
+conn.login(username, password, function(err, userInfo) {
+  if (err) { return console.error(err); }
+  console.log("SOQL FITCH RESULT")
+  let q = 'SELECT id, Site, name FROM account LIMIT 2';
+  conn.query(q, function(err, result1) {
+    if (err) { return console.error(err); }
+    //console.log("total : " + result1.totalSize);
+    //console.log("fetched : " + result1.records.length);
+    //console.log(result1);
+    res.send(result1)
+  });
+  
+}); })
+
+ //CONDITIONAL QUERY
+app.get('/forceapi/Records2', (req, res) => {
+  conn.login(username, password, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    console.log("SOQL FITCH RESULT")
+    let  q = "SELECT Id, Name, CreatedDate FROM Contact WHERE CreatedDate >= YESTERDAY ORDER BY CreatedDate DESC, Name ASC LIMIT 5 OFFSET 10"
+    conn.query(q, function(err, result2) {
+      if (err) { return console.error(err); }
+      //console.log(result2);
+      res.send(result2)
+    });
+    
+  }); })
+//############################## Query Method-Chain(JSON): Fetch Contact's records.################################
+//using SObject#find(conditions, fields), you can do query in JSON-based condition expression 
+
+conn.login(username, password, function(err, userInfo) {
+  if (err) { return console.error(err); }
+  //console.log("JSON FITCH RESULT")
+  conn.sobject("Contact").find(
+    // conditions in JSON object
+    {  CreatedDate: { $gte : jsforce.Date.YESTERDAY }},
+    // fields in JSON object
+    { Id: 1,
+      Name: 1,
+      CreatedDate: 1 }
+  )
+  .sort({ CreatedDate: -1, Name : 1 })
+  .limit(5)
+  .skip(10)
+  .execute(function(err, records) {
+    if (err) { return console.error(err); }
+    //console.log("fetched : " + records.length);
+  });
+})
 
 //***************************************************************************** */
 //HTTP REQUESTS
-//User authentification
-/*app.get('/forceapi/authen', (req,res) => {
-  conn.login(username, password, function(err, res) {
-    if (err) { return console.error(err); }
-    conn.query('SELECT Id, Name FROM Account', function(err, user) {
-      if (err) { return console.error(err); }
-      console.log(user);
-    });
-  });
-  res.send(user)
-})
 //CRUD
-//1. Retreive records using their IDs
+//1. Retreive record from Account using an ID
 //Record Detail page
-// Single record retrieval
-
-recordExample = "0017000000hOMChAAO"
+/*recordExample = "0018a00001otYcdAAE"
 app.get('/forceapi/record/detail/:recordID', (req, res) => {
-  conn.sobject("Account").retrieve(req.params.id , function(err, account) {
+  conn.sobject("Account").retrieve(parseInt(req.params.recordID) , function(err, account) {
     if (err) { return console.error(err); }
     console.log("Name : " + account.Name);
     // ...
   })
-  res.send(account.Name)
-})
+  res.send(res)
+})*/
 // Multiple record retrieval
 
-
+/*
 //2. Create Records
 app.post('/forceapi/records',(req,res)=>{
   
